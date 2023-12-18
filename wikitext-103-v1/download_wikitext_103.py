783ed2d93c5efd912cc5dcf65412bd8181fce02f
jinda jia <62297091+jindajia@users.noreply.github.com>
2023-12-18 12:19:25 -0500

    Update download_wikitext_103.py

--- wikitext-103-v1/download_wikitext_103.py
+++ wikitext-103-v1/download_wikitext_103.py
@@ -7,5 +7,6 @@ disable_caching()
 train_data = load_dataset('wikitext', name='wikitext-103-v1', split='train')
 train_data.to_json("wikitext-103-v1.json", lines=True)  
 
+# download from https://huggingface.co/gpt2/tree/main
 hf_hub_download(repo_id="gpt2", filename="merges.txt", local_dir='./')
 hf_hub_download(repo_id="gpt2", filename="vocab.json", local_dir='./')
